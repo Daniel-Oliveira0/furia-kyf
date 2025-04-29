f1a9d76818f4b471fd8ea1c07e4b788002a8043d
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-04-28 21:11:24 -0300

    adicionado as rotas das páginas

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Faça login para acessar o sistema.</p>
+    </div>
+  );
+}
+
+export default Login;
