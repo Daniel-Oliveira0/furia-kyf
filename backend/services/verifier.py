98d5b2547d6347dded8b4a912af2a4e5472a8e93
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-05-01 11:18:29 -0300

    adicionado a verificação de mais arquivos e de comprovante de residência

--- backend/services/verifier.py
+++ backend/services/verifier.py
@@ -1,5 +1,15 @@
+import os
+
 def verify_document(file_path: str):
-    if "identity" in file_path.lower():
-        return "Documentos de identidade válidos"
+    file_name, file_extension = os.path.splitext(file_path)
+    
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.pdf']
+    if file_extension.lower() not in valid_extensions:
+        return "Extensão inválida. Apenas JPG, PNG e PDF são permitidos."
+    
+    if "identidade" in file_name.lower():
+        return "Documento de identidade válido"
+    elif "comprovante_residencia" in file_name.lower():
+        return "Comprovante de residência válido"
     else:
-        return "Documento inválido"
+        return "Documento não reconhecido."
